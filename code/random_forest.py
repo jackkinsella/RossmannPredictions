7ae9957752bd997e88969f55442164928fc0638d
Jack Kinsella <151206+jackkinsella@users.noreply.github.com>
2019-07-14 16:16:26 +0200

    Function to detect overfitting trends

--- code/random_forest.py
+++ code/random_forest.py
@@ -1,7 +1,7 @@
 from sklearn.ensemble import RandomForestRegressor
 
 from prepare_data import X, y
-from utils import root_mean_square_percentage_error
+from utils import root_mean_square_percentage_error, compare_train_test_error
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -9,8 +9,14 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Ensure we are not using future to predict past
 assert X_train["Year"].max() <= X_test["Year"].min()
 
-model = RandomForestRegressor(n_estimators=5, random_state=42, n_jobs=-1)
+model = RandomForestRegressor(
+    n_estimators=10, max_depth=1000, random_state=42, n_jobs=-1)
 model.fit(X, y)
 
 error = root_mean_square_percentage_error(model.predict(X_test), y_test)
 print(f"Root mean squared percentage error: {error}")
+
+print(
+
+    compare_train_test_error(X_train, X_test, y_train, y_test, model)
+)
--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 
+def compare_train_test_error(X_train, X_test, y_train, y_test, model):
+    """See if the model is overfitting"""
+    train_error = root_mean_square_percentage_error(
+        model.predict(X_train), y_train)
+    print("Train error is: ", train_error)
+    test_error = root_mean_square_percentage_error(
+        model.predict(X_test), y_test)
+    return train_error - test_error
+
+
 def root_mean_square_percentage_error(y_pred, y_true):
     non_zero_indexes = np.where(y_true != 0)
     y_true_no_zeros = y_true.values[non_zero_indexes]
