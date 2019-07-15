7463ea15d55a63788e90e024b41066f1f1403ad1
Jack Kinsella <151206+jackkinsella@users.noreply.github.com>
2019-07-15 10:53:37 +0200

    Move notebooks to own folder

--- code/linear_regression.py
+++ code/linear_regression.py
@@ -5,10 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.dummy import DummyRegressor
 
-X_train, X_test, y_train, y_test = train_test_split(X,
-                                                    y,
-                                                    test_size=0.3,
-                                                    shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, shuffle=False)
 
 assert X_train["Year"].max() <= X_test["Year"].min()
 
